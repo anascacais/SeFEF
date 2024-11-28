cd1d0d3044822395f10b7a99e5e7778f9f49ad3a
anascacais <anascacais@gmail.com>
2024-11-28 09:48:46 +0000

    adapted setup.py to include requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup, find_packages
 
+
+with open("requirements.txt") as f:
+    required = f.read().splitlines()
+
+
 setup(
     name='sefef',
     version='0.0.1',
     license="MIT license",
     description='SeFEF: A Seizure Forecast Evaluation Framework',
-    readme="README.md",
+    readme="README.rst",
     author="Ana Sofia Carmo",
     author_email="anascacais@gmail.com",
     packages=find_packages(include=['sefef', 'sefef.*']),
+    install_requires=required,
     setup_requires=['pytest-runner', 'flake8'],
     test_suite="tests",
     tests_require=['pytest'],
