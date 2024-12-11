bc96a1742bcc65e38eed354fe0cc6b8fff738718
anascacais <anascacais@gmail.com>
2024-12-11 16:14:14 +0000

    updated version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def read_readme():
 
 setup(
     name='sefef',
-    version='1.3.0',
+    version='1.2.1',
     license="BSD 3-clause",
     description='SeFEF: Seizure Forecast Evaluation Framework',
     long_description=read_readme(),
